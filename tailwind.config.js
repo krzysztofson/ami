2521b45204380b8a02bd806e91f8af9460691118
Krzysztof Dabrowski <krzysztofson@gmail.com>
2025-04-24 13:14:33 +0200

    template

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        main: ["Poppins", "sans-serif"],
+        heading: ["Playfair Display", "serif"],
+      },
+      colors: {
+        primary: "#3c6ca8",
+        secondary: "#8199bd",
+        accent: "#3f3f3f",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
